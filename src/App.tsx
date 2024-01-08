49e9e89eae3edd2e2e239c51d41ec8398bc60954
Isa Amerkhanov <isa.amerkhanov@gmail.com>
2024-01-08 13:56:08 +0300

    Создана структура проекта

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './scss/main.scss'
+import styles from './App.module.scss'
+
+function App() {
+  return (
+    <div className={styles.container}>
+    </div>
+  );
+}
+
+export default App;
